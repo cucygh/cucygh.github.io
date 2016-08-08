5fc39bf1d845479ea1e21878835c38481ad5340d
yinguohui <yinguohui@bytedance.com>
2016-08-08 23:29:05 +0800

    Site updated: 2016-08-08 23:29:05

--- post.js
+++ post.js
@@ -1,1 +1,9 @@
-document.write('<p>postscribe</p>')
+console.log('post');
+document.write('<p>kkkk</p>')
+// var s=document.createElement('script');
+// s.text='document.write("<p>kkkk</p>")'
+// document.getElementById('test').appendChild(s);
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     document.write('<p>ssss</p>')
+//
+//   });
