aa884f2f635efa03c185ba46b0aceabafbd758cd
yinguohui <yinguohui@bytedance.com>
2016-08-21 11:16:00 +0800

    Site updated: 2016-08-21 11:16:00

--- post.js
+++
@@ -1,9 +0,0 @@
-console.log('post');
-document.write('<p>kkkk</p>')
-// var s=document.createElement('script');
-// s.text='document.write("<p>kkkk</p>")'
-// document.getElementById('test').appendChild(s);
-// document.addEventListener("DOMContentLoaded", function(event) {
-//     document.write('<p>ssss</p>')
-//
-//   });
